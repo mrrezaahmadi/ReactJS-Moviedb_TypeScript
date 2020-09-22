2636b2559aeefe2c0879de1b7f091fc5754a86bc
mrrezaahmadi <mrrezaahmadi>
2020-09-22 17:33:00 +0330

    add button to load more items in page

---
+++ src/components/load-more-button/load-more-btn.component.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+// Styles
+import "./load-more-btn.styles.scss";
+
+interface LoadMoreBtnPropsTypes {
+	loadMoreButtonHandler: any;
+}
+
+const LoadMoreBtn: React.FC<LoadMoreBtnPropsTypes> = (props) => {
+	const { loadMoreButtonHandler } = props;
+	return (
+		<div className="load-more-btn" onClick={loadMoreButtonHandler}>
+			<p>Load More Movies ...</p>
+		</div>
+	);
+};
+
+export default LoadMoreBtn;
--- src/pages/home-page/home-page.component.tsx
+++ src/pages/home-page/home-page.component.tsx
@@ -10,15 +10,18 @@ import {
 } from "../../constants/tmdb.config";
 
 // Styles
-import './home-page.styles.scss'
+import "./home-page.styles.scss";
 
 // Components
 import Poster from "../../components/poster/poster.component";
 import MovieList from "../../components/movie-list/movie-list.component";
+import LoadMoreBtn from "../../components/load-more-button/load-more-btn.component";
 
 const Home: React.FC = () => {
 	const [movies, setMovies] = useState<any>([]);
 	const [poster, setPoster] = useState<any>(null);
+	const [currentPage, setCurrentPage] = useState<any>(0);
+	const [totalPages, setTotalPages] = useState<any>(0);
 
 	useEffect(() => {
 		const endPoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
@@ -31,7 +34,16 @@ const Home: React.FC = () => {
 			.then((result) => {
 				setMovies([...movies, ...result.results]);
 				setPoster(poster || result.results[0]);
+				setCurrentPage(result.page);
+				setTotalPages(result.total_pages);
 			});
+	};
+
+	const loadMoreItems = () => {
+		let endPoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${
+			currentPage + 1
+		}`;
+		fetchItems(endPoint);
 	};
 
 	return (
@@ -47,6 +59,9 @@ const Home: React.FC = () => {
 			) : null}
 			<div className="movie-list-grid">
 				<MovieList movies={movies} />
+				{currentPage <= totalPages ? (
+					<LoadMoreBtn loadMoreButtonHandler={loadMoreItems} />
+				) : null}
 			</div>
 		</div>
 	);
