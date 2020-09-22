12d9d4be2d7c33b3e3d245544739be30a9924169
mrrezaahmadi <mrrezaahmadi>
2020-09-22 12:15:11 +0330

    initial commit

---
+++ src/pages/home-page/home-page.component.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './home-page.styles.scss'
+
+const Home: React.FC = () => {
+    return (
+        <div>
+            This is Home Page
+        </div>
+    )
+}
+
+export default Home
