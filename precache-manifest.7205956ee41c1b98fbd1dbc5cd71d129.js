251dabe4c092a0db0f67cfa1989fbe3b6248fb95
mrrezaahmadi <mrrezaahmadi>
2020-09-24 16:13:39 +0330

    Updates

---
+++ precache-manifest.7205956ee41c1b98fbd1dbc5cd71d129.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edc9c00a4e40a8f8ef864df66b5fa9cb",
+    "url": "/ReactJS-Moviedb_TypeScript/index.html"
+  },
+  {
+    "revision": "a895b50bb179b803d8cb",
+    "url": "/ReactJS-Moviedb_TypeScript/static/css/main.275b2b0e.chunk.css"
+  },
+  {
+    "revision": "fc0f5fc80276ef968d6c",
+    "url": "/ReactJS-Moviedb_TypeScript/static/js/2.e5cef3f6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactJS-Moviedb_TypeScript/static/js/2.e5cef3f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a895b50bb179b803d8cb",
+    "url": "/ReactJS-Moviedb_TypeScript/static/js/main.2909d94d.chunk.js"
+  },
+  {
+    "revision": "47986ada0ee73d5b6d22",
+    "url": "/ReactJS-Moviedb_TypeScript/static/js/runtime-main.0926d48f.js"
+  }
+]);
